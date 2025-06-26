b049373a7afa7642fbb84bd52ba24f408725f770
JeanPhilippeCaetano <caetano.jphilippe@gmail.com>
2025-06-26 14:39:03 +0200

    add bonus

---
+++ BladeMatch/Services/TournamentRanking.cs
@@ -0,0 +1,37 @@
+﻿using BladeMatch.Models;
+
+namespace BladeMatch.Services;
+
+public class TournamentRanking
+{
+    private readonly ScoreCalculator _scoreCalculator;
+    public TournamentRanking(ScoreCalculator scoreCalculator)
+    {
+        _scoreCalculator = scoreCalculator;
+    }
+    /// <summary>
+    /// Classe les joueurs par score décroissant
+    /// </summary>
+    public List<Player> GetRanking(List<Player> players)
+    {
+        if (players.Count == 0)
+        {
+            throw new Exception("No players found");
+        }
+        players.Sort((a, b) => b.Score.CompareTo(a.Score));
+        return players;
+    }
+    /// <summary>
+    /// Trouve le champion (joueur avec le meilleur score)
+    /// </summary>
+    public Player GetChampion(List<Player> players)
+    {
+        if (players.Count == 0)
+        {
+            throw new Exception("No players found");
+        }
+        players.Sort((a, b) => b.Score.CompareTo(a.Score));
+        return players[0];
+    }
+
+}
